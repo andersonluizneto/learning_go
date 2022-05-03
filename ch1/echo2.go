1ff44d4a00ec9bc8faecb840807b3e1163d57e64
Anderson Luiz Neto <anderson.neto26@gmail.com>
2022-05-03 12:54:03 -0300

    Echo program v2

---
+++ ch1/echo2.go
@@ -0,0 +1,16 @@
+// Echo2 show your command lines arguments
+package main
+
+import (
+	"fmt"
+	"os"
+)
+
+func main()  {
+	s, sep := " ", " "
+
+	for _, arg := range os.Args[1:] {
+		s += sep + arg
+	}
+	fmt.Println(s)
+}
