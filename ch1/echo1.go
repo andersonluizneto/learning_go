a62091981179b61a93cd57bc46ed9b45c8e8ad10
Anderson Luiz Neto <anderson.neto26@gmail.com>
2022-05-03 12:07:10 -0300

    Echo version 1

---
+++ ch1/echo1.go
@@ -0,0 +1,17 @@
+// Echo1 show your commands lines arguments.
+
+package main
+
+import (
+	"fmt"
+	"os"
+)
+
+func main()  {
+	var s, sep string
+	for i := 0; i < len(os.Args); i++ {
+		s += sep + os.Args[i]
+		sep = " "
+	}
+	fmt.Println(s)
+}
